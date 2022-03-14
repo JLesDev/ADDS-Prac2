e5bed30be4689d5b017bd7049107e9dc6182964f
Jonty Leslie <jontyleslie@gmail.com>
2022-03-14 15:59:46 +1030

    Testing adding 1 file at a time

---
+++ Human.h
@@ -0,0 +1,21 @@
+#ifndef Human_hpp
+#define Human_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+class Human{
+        
+    private:
+        
+    public:
+    
+        Human();
+        char makeMove();  
+    
+    
+};
+
+#endif /*Human_hpp */
