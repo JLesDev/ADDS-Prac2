a311e3b4a4be756726f921daea9f222b7030b3c4
Jonty Leslie <jontyleslie@gmail.com>
2022-03-14 16:02:25 +1030

    Worked. Adding final files.

---
+++ Computer.cpp
@@ -0,0 +1,15 @@
+#include "Computer.h"
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+Computer::Computer(){
+
+};
+
+char Computer::makeMove(){
+    
+};
---
+++ Computer.h
@@ -0,0 +1,21 @@
+#ifndef Computer_hpp
+#define Computer_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+class Computer{
+        
+    private:
+        
+    public:
+    
+        Computer();
+        char makeMove();  
+    
+    
+};
+
+#endif /*Human_hpp */
