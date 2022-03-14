4828222ccfdedfa2ea51ce1e9e2661a0f026fc74
Jonty Leslie <jontyleslie@gmail.com>
2022-03-14 15:54:25 +1030

    Adding base files for ADDS Practical 2

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Human.h"
+#include "Computer.h"
+#include "Referee.h"
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(){
+
+    
+    return 0;
+};
+
